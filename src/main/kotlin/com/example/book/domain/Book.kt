53a5dac8fcfd4b34bd33c5c08ec400c7ed3cfa32
aerkinbekkyzy <yar30m1993g>
2019-08-10 17:46:58 +0600

    initial commit

---
+++ src/main/kotlin/com/example/book/domain/Book.kt
@@ -0,0 +1,18 @@
+package com.example.book.domain
+import javax.persistence.*
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Entity
+@Table(name = "books")
+class Book(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        var id: Long? = null,
+
+        @NotBlank
+        @Size(min = 1, max = 4000)
+        @Column(name = "name")
+        var name: String
+)
---
+++ src/main/resources/migrations/insert-book-table.sql
@@ -0,0 +1,8 @@
+INSERT INTO books (id, name)
+VALUES (1, 'Java Programming 24-Hour Trainer');
+
+INSERT INTO books (id, name)
+VALUES (2, 'Java 8 in Action');
+
+INSERT INTO books (id, name)
+VALUES (3, 'Head First Java');
